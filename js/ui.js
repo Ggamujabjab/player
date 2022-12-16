081916b161c6ac74a857cf176be2bc352e531d8d
jk9804034 <jk9804034@naver.com>
2022-12-16 14:32:36 +0900

    영상 로드후 기본 기능

--- js/ui.js
+++ js/ui.js
@@ -65,11 +65,33 @@ var PlayerCustomizing = function(content, m){
 
     var that = this
 	, videoForm = this.videoUiCreate(content, m) // 비디오 UI 생성
-    , videoLoading = this.loadingUiCreate({ cont : content }); // 로딩바 생성
+    , videoLoading = this.loadingUiCreate({ cont : content }) // 로딩바 생성
+    , bnnrFlag = false
+	, $completeLayer;
 
     // 전역적으로 사용하기 위한 변수(버튼 요소)
 	this.videoForm = videoForm; // 생성된 video
     this.content = $(content); // 전체 영역
+    this.title = $(".py_title"); // 타이틀
+    this.layerPlay = $(".py_btn_playing") // 플레이 레이어
+	this.btnPlay = $(".py_play_control"); // 시작, 정지 버튼
+	this.btnPrev = $(".py_prev_control"); // 10초전 버튼
+	this.btnNext = $(".py_next_control"); // 10초후 버튼
+	this.volumePanel = $(".py_volume_panel"); // 볼륨
+	this.volumeButton = this.volumePanel.find(".py_mute_control") // 볼륨 버튼
+	this.volumeProgBar = this.volumePanel.find(".py_volume_bar") // 볼륨
+	this.progCurrentTime = $(".py_current_time"); // 현재시간
+	this.controllBox = $(".py_controlbox_wrap") // 컨트럴박스
+	this.progressBar = $(".py_progress_bar"); // 재생 상태바
+	this.progDurationTime = $(".py_duration_time"); // 완료시간
+	this.btnListnext = $(".py_listNext_control"); // 다음화 버튼
+	this.btnListseries = $(".py_listSeries_control"); // 리스트 목록 버튼
+	this.btnFullscreen = $(".py_fullscreen_control"); // 전체보기 버튼
+	this.seriesList = $(".py_series_list") // 리스트 목록 레이어
+	this.playerClose = $(".py_btn_close a") // 플레이어 닫기
+	// 전역적으로 사용하기 위한 변수(시간 요소)
+	this.timeCurrent = 0;
+	this.timeDuration = 0;
 
     // 로딩 시작
 	$(this.videoForm).off("seeking");
@@ -115,7 +137,318 @@ var PlayerCustomizing = function(content, m){
 PlayerCustomizing.prototype = {
 	// 초기 작업
 	initAction : function(m){
-        console.log("시작");
+        var that = this
+		, eleContent = m.wrap;
+
+        // 비디오 전체영역 클릭
+		$(this.videoForm).on("click", function(e){
+			if( !touchtime ){
+				touchtime = true;
+
+				dbEventTime = setTimeout(function(){
+					touchtime = false;
+
+					if( timer ){
+						clearTimeout( timer );
+					}
+
+					// 컨트럴러 활성화 여부
+					if( $(eleContent).hasClass("py_user_active") ){
+						$(eleContent).removeClass("py_user_active");
+
+						// 재생중
+						if( $(eleContent).hasClass("py_playing") ){
+							timer = that.userControlFlag({ wrap : eleContent });
+						}
+					} else {
+						$(eleContent).addClass("py_user_active");
+					}
+				}, 200);
+			} else {
+				clearTimeout(dbEventTime);
+
+				touchtime = false;
+
+				if( e.originalEvent.pageX >= $(window).width()-Math.floor($(window).width()/2) ){
+					// 재생중인지 체크
+					if( $(eleContent).hasClass("py_playing") ){
+						if( timer ){
+							clearTimeout( timer );
+						}
+
+						// 컨트럴러 숨김 기능
+						timer = that.userControlFlag({ wrap : eleContent });
+					}
+
+					if( $(eleContent).hasClass("py_user_active") ){
+						$(eleContent).append( that.btnNext );
+					}
+
+					that.btnNext.addClass("py_next_control_active");
+					setTimeout(() => {
+						that.btnNext.removeClass("py_next_control_active");
+
+						if( $(eleContent).hasClass("py_user_active") ){
+							that.btnPrev.after( that.btnNext );
+						}
+					}, 500);
+
+					// 현재시간을 기준으로 10초후으로 적용
+					that.videoForm.currentTime = that.videoForm.currentTime + that.opt.overTimeUse;
+				} else if( e.originalEvent.pageX <= $(window).width()-(Math.floor($(window).width()/2))) {
+					// 재생중인지 체크
+					if( $(eleContent).hasClass("py_playing") ){
+						if( timer ){
+							clearTimeout( timer );
+						}
+
+						// 컨트럴러 숨김 기능
+						timer = that.userControlFlag({ wrap : eleContent });
+					}
+
+					if( $(eleContent).hasClass("py_user_active") ){
+						$(eleContent).append( that.btnPrev );
+					}
+
+					that.btnPrev.addClass("py_prev_control_active");
+					setTimeout(() => {
+						that.btnPrev.removeClass("py_prev_control_active");
+
+						if( $(eleContent).hasClass("py_user_active") ){
+							that.btnNext.before( that.btnPrev );
+						}
+					}, 500);
+
+					// 현재시간을 기준으로 10초전으로 적용
+					that.videoForm.currentTime = that.videoForm.currentTime - that.opt.overTimeUse;
+				}
+			}
+
+			return false;
+		});
+
+        // 타이틀 마우스 오버
+		this.title.on("mouseenter", function(){
+            // 재생중인지 체크
+			if( $(eleContent).hasClass("py_playing") && !that.seriesList.hasClass("py_series_list_active") ){
+				// 전체 컨트럴러 사라짐 타이머
+				if( timer ){
+					clearTimeout( timer );
+				}
+			}
+		});
+        // 타이틀 마우스 아웃
+		this.title.on("mouseleave", function(){
+            // 재생중인지 체크
+			if( $(eleContent).hasClass("py_playing") && !that.seriesList.hasClass("py_series_list_active") ){
+				// 컨트럴러 숨김 기능
+				timer = that.userControlFlag({ wrap : eleContent });
+			}
+		});
+
+        // 컨트롤러 마우스 오버
+		this.controllBox.on("mouseenter", function(){
+            // 재생중인지 체크
+			if( $(eleContent).hasClass("py_playing") && !that.seriesList.hasClass("py_series_list_active") ){
+				// 전체 컨트럴러 사라짐 타이머
+				if( timer ){
+					clearTimeout( timer );
+				}
+			}
+		});
+        // 컨트롤러 마우스 아웃
+		this.controllBox.on("mouseleave", function(){
+            // 재생중인지 체크
+			if( $(eleContent).hasClass("py_playing") && !that.seriesList.hasClass("py_series_list_active") ){
+				// 컨트럴러 숨김 기능
+				timer = that.userControlFlag({ wrap : eleContent });
+			}
+		});
+
+        // 플레이어 닫기 버튼
+		this.playerClose.on("click", function(){
+			self.close();
+		});
+		// 플레이어 마우스 오버
+		this.playerClose.on("mouseenter", function(){
+			if( $(eleContent).hasClass("py_playing") ){
+				// 전체 컨트럴러 사라짐 타이머
+				if( timer ){
+					clearTimeout( timer );
+				}
+			}
+		});
+		// 플레이어 마우스 아웃
+		this.playerClose.on("mouseleave", function(){
+            // 재생중인지 체크
+			if( $(eleContent).hasClass("py_playing") ){
+				// 컨트럴러 숨김 기능
+				timer = that.userControlFlag({ wrap : eleContent });
+			}
+		});
+
+        // 메인 플레이 레이어 오버
+        this.layerPlay.on("mouseenter", function(){
+            $(this).addClass("py_btn_playing_active");
+        });
+        // 메인 플레이 레이어 아웃
+        this.layerPlay.on("mouseleave", function(){
+            $(this).removeClass("py_btn_playing_active");
+        });
+        // 메인 플레이 레이어 클릭
+        this.layerPlay.on("click", function(){
+            // 재생
+            that.videoForm.play();
+
+            // 숨김
+            $(this).addClass("py_btn_playing_hide");
+
+            // 컨트럴러 숨김 기능
+            timer = that.userControlFlag({ wrap : eleContent });
+        });
+
+        // 재생 및 정지 버튼 클릭
+		this.btnPlay.on("click", function(){
+			// 전체 컨트럴러 사라짐 타이머
+			if( timer ){
+				clearTimeout( timer );
+			}
+
+            // 목록보기 리스트가 활성화 되어있을경우
+            if( that.seriesList.hasClass("py_series_list_active") ){
+                that.seriesList.removeClass("py_series_list_active");
+            }
+
+            // 재생중인지 체크
+			if( $(eleContent).hasClass("py_playing") ){
+				// 정지
+				that.videoForm.pause();
+
+				// 정지일경우 전체 영역에 클래스 삭제
+				$(eleContent).removeClass("py_playing");
+				$(eleContent).removeClass("py_user_active");
+                // 버튼 레이어 숨김
+                that.layerPlay.removeClass("py_btn_playing_hide");
+
+				// 데이터 저장(현재 시간 저장)
+				that.postDataSend({ time : that.timeCurrent, tTime : that.timeDuration });
+			} else {
+				// 재생
+				that.videoForm.play();
+                // 버튼 레이어 숨김
+                that.layerPlay.addClass("py_btn_playing_hide");
+			}
+
+			return false;
+		});
+
+        // 10초전 이동 버튼 클릭
+		this.btnPrev.on("click", function(){
+			// 재생중인지 체크
+			if( $(eleContent).hasClass("py_playing") ){
+				if( timer ){
+					clearTimeout( timer );
+				}
+			}
+
+			$(this).addClass("py_prev_control_active");
+			setTimeout(() => {
+				$(this).removeClass("py_prev_control_active");
+			}, 300);
+
+			// 현재시간을 기준으로 10초전으로 적용
+			that.videoForm.currentTime = that.videoForm.currentTime - that.opt.overTimeUse;
+
+			return false;
+		});
+
+        // 10초후 이동 버튼 클릭
+		this.btnNext.on("click", function(){
+			// 재생중인지 체크
+			if( $(eleContent).hasClass("py_playing") ){
+				if( timer ){
+					clearTimeout( timer );
+				}
+			}
+
+			$(this).addClass("py_next_control_active");
+			setTimeout(() => {
+				$(this).removeClass("py_next_control_active");
+			}, 300);
+
+			// 현재시간을 기준으로 10초후으로 적용
+			that.videoForm.currentTime = that.videoForm.currentTime + that.opt.overTimeUse;
+
+			return false;
+		});
+
+        // 전체보기 버튼 클릭
+		this.btnFullscreen.on("click", function(){
+			// 전체 컨트럴러 활성화
+			if( $(eleContent).hasClass("py_playing") ){
+				if( timer ){
+					clearTimeout( timer );
+				}
+			}
+
+			// 전체화면 함수
+			that.fullSizeFlag({ content : eleContent });
+		});
+
+        // 전체보기 ESC키 이벤트
+        document.addEventListener("fullscreenchange", evtFullEsc);
+        document.addEventListener("webkitfullscreenchange", evtFullEsc);
+        document.addEventListener("mozfullscreenchange", evtFullEsc);
+        document.addEventListener("MSFullscreenChange", evtFullEsc);
+
+        // 키보드 클릭
+		$(window).on("keyup", function(e){
+			if( e.keyCode == 37 ){ // 좌
+				// 전체 컨트럴러 사라짐 타이머
+				if( timer ){
+					clearTimeout( timer );
+				}
+
+				// 현재시간을 기준으로 10초전으로 적용
+				that.videoForm.currentTime = that.videoForm.currentTime - that.opt.overTimeUse;
+
+				// 컨트럴러 숨김 기능
+				timer = that.userControlFlag({ wrap : eleContent });
+			} else if( e.keyCode == 39 ){ // 우
+				// 전체 컨트럴러 사라짐 타이머
+				if( timer ){
+					clearTimeout( timer );
+				}
+
+				// 현재시간을 기준으로 10초후으로 적용
+				that.videoForm.currentTime = that.videoForm.currentTime + that.opt.overTimeUse;
+
+				// 컨트럴러 숨김 기능
+				timer = that.userControlFlag({ wrap : eleContent });
+			} else if( e.keyCode == 32 ){ // 스페이스바
+				that.btnPlay.trigger("click");
+			} else if( e.keyCode == 13 ){ // 엔터
+				that.btnFullscreen.trigger("click");
+            }
+		});
+
+        // 전체보기 ESC키 인식 이벤트
+        function evtFullEsc(){
+            // 엣지 및 크롬
+            if( document.webkitIsFullScreen == false ){
+                that.content.removeClass("py_fullsize_active");
+            }
+            // 파이어폭스
+            if( document.mozFullScreen == false ){
+                that.content.removeClass("py_fullsize_active");
+            }
+            // 익스 하위버전
+            if( document.msFullscreenEnabled == true ){
+                if( document.msFullscreenElement == null ){
+                    that.content.removeClass("py_fullsize_active");
+                }
+            }
+        }
     },
     // video 생성
 	videoUiCreate : function(content, m){
@@ -450,13 +783,44 @@ PlayerCustomizing.prototype = {
 
 		return $video[0];
 	},
+    // 컨트롤박스 활성화 여부
+	userControlFlag : function(m){
+		var controlTimer = setTimeout(() => {
+			$(m.wrap).addClass("py_user_active");
+		}, 3000);
+
+		return controlTimer;
+	},
     // 데이터 로딩바
 	loadingUiCreate : function(m){
 		var $spinner = $("<div />").addClass("py_loading_spinner")
 			.appendTo( $(m.cont) );
 
 		return $spinner;
-	}
+	},
+    // 데이터 전송
+	postDataSend : function(m){
+		var time = m.time
+		, totalTime = m.tTime;
+
+		/*$.ajax({
+			type : "POST", // 호출 방식
+			url : "/api/down/player_time.php" + "?link_idx=" + this.opt.initInfo.link_idx + "&no=" + this.opt.initInfo.no + "&wp_idx=" + this.opt.initInfo.wp_idx + "&playtime=" + time + "&totaltime=" + totalTime, // 호출 URL
+			async : true, // 동기, 비동기 방식(true : 비동기, false : 동기)
+			timeout : 5000,
+			data : {},
+			dataType : "json",
+			jsonpCallback: false,
+	        crossDomain: false,
+	        contentType: false,
+			success : function(response){
+				console.log(response);
+			},
+			error : function(error){
+				console.log(error);
+			}
+		});*/
+	},
 }
 // 옵션
 $.fn.playerCustomizing.default = {
